3d000e976ad49911d9b58a059c12e7e87859f3cd
sandaru <sandaru.14@cse.mrt.ac.lk>
2016-11-19 21:36:25 +0530

    DB classes added

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
@@ -0,0 +1,10 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+
+/**
+ * Created by Userpc on 11/19/2016.
+ */
+
+public class PersistentAccountDAO implements AccountDAO {
+}
